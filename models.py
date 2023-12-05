5cb98953a995d626b61c765c8343b7868ee5b64a
Sebactian Monroe <152859515+swamps-matrix@users.noreply.github.com>
2023-12-05 13:21:24 +0600

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
